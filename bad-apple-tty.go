48a530d45cff5e1bfc5fc1d0209a69f4e1d844c4
Uzen Huang <huang573@wisc.edu>
2023-03-19 22:58:16 -0500

    refactored code to gocv + added tcell terminal size checking

--- bad-apple-tty.go
+++ bad-apple-tty.go
@@ -2,18 +2,16 @@ package main
 
 import (
 	"fmt"
+	"image"
 	"image/color"
-	"image/png"
-	"io"
-	"io/ioutil"
 	"os"
-	"os/exec"
 	"time"
 	"unicode/utf8"
 	"strconv"
 	"flag"
 	"strings"
 	"gocv.io/x/gocv"
+	"github.com/gdamore/tcell/v2"
 )
 
 // For parsing command line arguments
@@ -21,20 +19,19 @@ var fps int
 var mode string
 var args []string
 var threshold int
-var video bool
+var screen tcell.Screen
 
 func init() {
     flag.Usage = func() {
         fmt.Fprintf(os.Stderr, "Usage: %s [options] [arguments]\n", os.Args[0])
-		fmt.Println("-args: \nFolder for images to load")
+		fmt.Println("-args: \nVideo to load")
         flag.PrintDefaults()
     }
 
 	// Parse the command line arguments
 	flag.IntVar(&fps, "f", 30, "fps to run at")
-	flag.StringVar(&mode, "m", "truecolor", "mode to run in (tty, unicode, truecolor")
+	flag.StringVar(&mode, "m", "truecolor", "mode to run in (tty, tty_subsample, unicode, truecolor")
 	flag.IntVar(&threshold, "t", 128, "threshold for quantization")
-	flag.BoolVar(&video, "v", false, "inputs from video")
 
 	flag.Parse()
 
@@ -50,299 +47,51 @@ func main() {
 		os.Exit(1)
 	}
 
-	folder := args[0]
+	// Initialize the screen
+	screen, err := tcell.NewScreen()
+    if err != nil {
+        panic(err)
+    }
+    if err := screen.Init(); err != nil {
+        panic(err)
+    }
+    defer screen.Fini()
 
-	if fps < 1 {
-		fmt.Println("Invalid fps")
-		flag.Usage()
-		os.Exit(1)
-	}
-
-	if threshold < 0 || threshold > 255 {
-		fmt.Println("Invalid threshold")
-		flag.Usage()
-		os.Exit(1)
-	}
-
-	// Clear the screen
-	cmd := exec.Command("clear")
-	cmd.Stdout = os.Stdout
-	cmd.Run()
-
-	// load a folder
-	var images []string
-	var err error
-	var videoIn string
-
-	interval := time.Second / time.Duration(fps)
-
-	// hide cursor
-	fmt.Print("\033[?25l")
-
-	if !video {
-		images, err = loadFolder(folder)
-		if err != nil {
-			panic(err)
-		}
-	} else {
-		videoIn = folder
-		runVideo(videoIn, interval)
-	}
-
-
-	// Parse the flags
-	switch mode {
-		case "tty":
-			runTty(images, interval)
-		case "tty-subsample":
-			runTtySubsample(images, interval)
-		case "unicode":
-			runUnicode(images, interval)
-		case "truecolor":
-			runTruecolor(images, interval)
-		default:
-			fmt.Println("Invalid mode")
-			flag.Usage()
-			os.Exit(1)
-	}
-
-	// show cursor
-	fmt.Print("\033[?25h")
+	// Run the program
+	videoMode(args[0])
 }
 
-// Run in tty mode
-func runTty(images []string, interval time.Duration) {
-	// loop through every image in the folder
-	for _, file := range images {
-		start := time.Now()
-
-		// Reset the cursor
-		fmt.Println("\033[H")
-
-		// Open the image
-		image, err := os.Open(file)
-		if err != nil {
-			panic(err)
-		}
-
-		// Import the image
-		pixels, height, width, err := importFrame(image)
-		if err != nil {
-			panic(err)
-		}
-
-		// Quantize the image
-		quantized := quantize(pixels, height, width, uint8(threshold))
-
-		// Print the image
-		printFullBlocks(quantized, height, width, 1)
-		
-		// Sleep for the remainder of the frame
-		elapsed := time.Since(start)
-
-		// Print the frame rate
-		fmt.Println("Theoretical FPS: " + strconv.Itoa(int(time.Second / elapsed)))
-
-		if elapsed < interval {
-			time.Sleep(interval - elapsed)
-		}
-
-		elapsed = time.Since(start)
-		fmt.Println("Real FPS: " + strconv.Itoa(int(time.Second / elapsed)))
-	}
-
-}
-
-// Run in tty-subsample mode
-func runTtySubsample(images []string, interval time.Duration) {
-	// loop through every image in the folder
-	for _, file := range images {
-		start := time.Now()
-
-		// Reset the cursor
-		fmt.Println("\033[H")
-
-		// Open the image
-		image, err := os.Open(file)
-		if err != nil {
-			panic(err)
-		}
-
-		// Import the image
-		pixels, height, width, err := importFrame(image)
-		if err != nil {
-			panic(err)
-		}
-
-		// Subsample the image
-		subsampled, height, width := subsample(pixels, height, width)
-
-		// Quantize the image
-		quantized := quantize(subsampled, height, width, uint8(threshold))
-
-		// Print the image
-		printFullBlocks(quantized, height, width, 1)
-		
-		// Sleep for the remainder of the frame
-		elapsed := time.Since(start)
-
-		// Print the frame rate
-		fmt.Println("Theoretical FPS: " + strconv.Itoa(int(time.Second / elapsed)))
-
-		if elapsed < interval {
-			time.Sleep(interval - elapsed)
-		}
-
-		elapsed = time.Since(start)
-		fmt.Println("Real FPS: " + strconv.Itoa(int(time.Second / elapsed)))
-	}
-
-}
-
-// Run in unicode mode
-func runUnicode(images []string, interval time.Duration) {
-	// loop through every image in the folder
-	for _, file := range images {
-		start := time.Now()
-
-		// Reset the cursor
-		fmt.Println("\033[H")
-
-		// Open the image
-		image, err := os.Open(file)
-		if err != nil {
-			panic(err)
-		}
-
-		// Import the image
-		pixels, height, width, err := importFrame(image)
-		if err != nil {
-			panic(err)
-		}
-
-		// Quantize the image
-		quantized := quantize(pixels, height, width, uint8(threshold))
-
-		// Print the image
-		printHalfBlocks(quantized, height, width)
-		
-		// Sleep for the remainder of the frame
-		elapsed := time.Since(start)
-
-		// Print the frame rate
-		fmt.Println("Theoretical FPS: " + strconv.Itoa(int(time.Second / elapsed)))
-
-		if elapsed < interval {
-			time.Sleep(interval - elapsed)
-		}
-
-		elapsed = time.Since(start)
-		fmt.Println("Real FPS: " + strconv.Itoa(int(time.Second / elapsed)))
-	}
-
-}
-
-
-// Run in truecolor mode
-func runTruecolor(images []string, interval time.Duration) {
-	// loop through every image in the folder
-	for _, file := range images {
-		start := time.Now()
-
-		// Reset the cursor
-		fmt.Println("\033[H")
-
-		// Open the image
-		image, err := os.Open(file)
-		if err != nil {
-			panic(err)
-		}
-
-		// Import the image
-		r, g, b, height, width, err := importFrameColor(image)
-		if err != nil {
-			panic(err)
-		}
-
-		printHalfBlocksColor(r, g, b, height, width)
-		
-		// Sleep for the remainder of the frame
-		elapsed := time.Since(start)
-
-		// Print the frame rate
-		fmt.Println("Theoretical FPS: " + strconv.Itoa(int(time.Second / elapsed)))
-
-		if elapsed < interval {
-			time.Sleep(interval - elapsed)
-		}
-
-		elapsed = time.Since(start)
-		fmt.Println("Real FPS: " + strconv.Itoa(int(time.Second / elapsed)))
-	}
-
-}
-
-// Run video
-func runVideo(videoIn string, interval time.Duration) {
-	// open the video
-	video, err := gocv.VideoCaptureFile(videoIn)
+// Runs the program by reading from a video
+func videoMode (fileName string) {
+	// Open the video
+	video, err := gocv.VideoCaptureFile(fileName)
 	if err != nil {
-		panic(err)
+		fmt.Println("Error opening video")
+		os.Exit(1)
 	}
 	defer video.Close()
 
-	// Loop through each frame in the video
+	// Calculate the frame interval
+	interval := time.Second / time.Duration(fps)
+
+	// Loop through the video
 	for {
-		// start the timer
+		// Start the timer
 		start := time.Now()
 
-		// Reset the cursor
-		fmt.Println("\033[H")
-
-		// read the next frame from the video
+		// Create a frame
 		frame := gocv.NewMat()
 		ok := video.Read(&frame)
 
 		if !ok {
-			// end of video
+			// end of the video
 			break
 		}
 
-		image, err := frame.ToImage()
-		if err != nil {
-			panic(err)
-		}
+		// Close the frame when done
+		defer frame.Close()
 
-		// Get the image bounds
-		bounds := image.Bounds()
-		width, height := bounds.Max.X, bounds.Max.Y
-
-		// Create a 2D array of the pixels
-		r := make([][]uint8, height)
-		g := make([][]uint8, height)
-		b := make([][]uint8, height)
-		a := make([][]uint8, height)
-
-		for i := range r {
-			r[i] = make([]uint8, width)
-			g[i] = make([]uint8, width)
-			b[i] = make([]uint8, width)
-			a[i] = make([]uint8, width)
-		}
-
-		// Iterate over the pixels and calculate the non premultiplied values
-		for y := 0; y < height; y++ {
-			for x := 0; x < width; x++ {
-				rgba := color.RGBAModel.Convert(image.At(x, y)).(color.RGBA)
-				r[y][x] = rgba.R
-				g[y][x] = rgba.G
-				b[y][x] = rgba.B
-				a[y][x] = rgba.A
-			}
-		}
-
-		// Print the image
-		printHalfBlocksColor(r, g, b, height, width)
+		processImage(frame)
 
 		// Print the frame rate
 		elapsed := time.Since(start)
@@ -355,146 +104,114 @@ func runVideo(videoIn string, interval time.Duration) {
 
 		elapsed = time.Since(start)
 		fmt.Println("Real FPS: " + strconv.Itoa(int(time.Second / elapsed)))
-
-		// Release the frame
-		frame.Close()
-	}	
+	}
 }
 
-// loads all png in a folder into an array of images
-func loadFolder(folder string) ([]string, error) {
-	
-	// get all the files in the folder
-	files, err := ioutil.ReadDir(folder)
+// Process an image and print it to the terminal
+func processImage (img gocv.Mat) {
+
+	// Convert the Mat to image, scaling to the terminal size
+	width, height := getTerminalSize()
+
+	if width == 0 || height == 0 {
+		fmt.Println("Error getting terminal size")
+		os.Exit(1)
+	}
+
+	switch mode {
+		case "tty":
+			// full blocks, we need to use double the width
+			width /= 2
+		case "tty_subsample":
+			// full blocks subsampled, two pixels per character
+			height *= 2
+		case "unicode":
+			// half blocks, two pixels high per character
+			height *= 2
+		case "truecolor":
+			// truecolor, same as unicode
+			height *= 2
+	}
+
+	resized := gocv.NewMat()
+	gocv.Resize(img, &resized, image.Point{X: width, Y: height}, 0, 0, gocv.InterpolationDefault)
+
+	// Get image from resized Mat
+	frame, err := resized.ToImage()
 	if err != nil {
-		return nil, err
+		fmt.Println("Error converting image")
+		os.Exit(1)
 	}
 
-	// create an array to hold the images
-	images := make([]string, len(files))
 
-	// loop through the files and add them to the array
-	for i, file := range files {
-		images[i] = folder + "/" + file.Name()
-	}
-
-	return images, nil
-}
-
-// Reads an image and returns a 2D array of the pixels in grayscale
-func importFrame(image io.Reader) ([][]uint8, int, int, error) {
-
-	// Open the image
-	file, err := png.Decode(image)
-	if err != nil {
-		return nil, 0, 0, err
-	}
-
-	// Get the image bounds
-	bounds := file.Bounds()
-	width, height := bounds.Max.X, bounds.Max.Y
-
-	// Create a 2D array of the pixels
-	pixels := make([][]uint8, height)
-	for i := range pixels {
-		pixels[i] = make([]uint8, width)
-	}
-
-	// Iterate over the pixels and convert them to grayscale
-	for y := 0; y < height; y++ {
-		for x := 0; x < width; x++ {
-			pixels[y][x] = color.GrayModel.Convert(file.At(x, y)).(color.Gray).Y
-		}
-	}
-
-	return pixels, height, width, nil
-}
-
-// Reads an image and return 3 2D arrays of the pixels in RGB, uint32
-func importFrameColor(image io.Reader) ([][]uint8, [][]uint8, [][]uint8, int, int, error) {
-	
-	// Open the image
-	file, err := png.Decode(image)
-	if err != nil {
-		return nil, nil, nil, 0, 0, err
-	}
-
-	// Get the image bounds
-	bounds := file.Bounds()
-	width, height := bounds.Max.X, bounds.Max.Y
-
-	// Create a 2D array of the pixels
-	r := make([][]uint8, height)
-	g := make([][]uint8, height)
-	b := make([][]uint8, height)
-	a := make([][]uint8, height)
-
-	for i := range r {
-		r[i] = make([]uint8, width)
-		g[i] = make([]uint8, width)
-		b[i] = make([]uint8, width)
-		a[i] = make([]uint8, width)
-	}
-
-	// Iterate over the pixels and calculate the non premultiplied values
-	for y := 0; y < height; y++ {
-		for x := 0; x < width; x++ {
-			rgba := color.RGBAModel.Convert(file.At(x, y)).(color.RGBA)
-			r[y][x] = rgba.R
-			g[y][x] = rgba.G
-			b[y][x] = rgba.B
-			a[y][x] = rgba.A
-		}
-	}
-
-	return r, g, b, height, width, nil
-}
-
-// convert an grayscale image to a quantized array
-func quantize(pixels [][]uint8, height int, width int, threshold uint8) [][]uint8 {
-
-	output := make([][]uint8, height)
-	for i := range output {
-		output[i] = make([]uint8, width)
-	}
-	
-	// loop through the pixels and quantize them
-	for y := 0; y < height; y++ {
-		for x := 0; x < width; x++ {
-			if pixels[y][x] < threshold {
-				output[y][x] = 0
-			} else {
-				output[y][x] = 1
+	// Print the image
+	switch mode {
+		case "tty":
+			// Convert the image to grayscale array
+			pixels, height, width, err := importFrame(frame)
+			if err != nil {
+				fmt.Println("Error converting image")
+				os.Exit(1)
 			}
-		}
+		
+			// Quantize the image
+			quantized := quantize(pixels, height, width, uint8(threshold))
+
+			// Print the image using fullblocks
+			printFullBlocks(quantized, height, width, 2)
+
+		case "tty_subsample":
+			// Convert the image to grayscale array
+			pixels, height, width, err := importFrame(frame)
+			if err != nil {
+				fmt.Println("Error converting image")
+				os.Exit(1)
+			}
+
+			// Subsample the image
+			subsampled, height, width := subsample(pixels, height, width)
+
+			// Quantize the image
+			quantized := quantize(subsampled, height, width, uint8(threshold))
+
+			// Print the image using fullblocks
+			printFullBlocks(quantized, height, width, 1)
+
+		case "unicode":
+			// Convert the image to grayscale array
+			pixels, height, width, err := importFrame(frame)
+			if err != nil {
+				fmt.Println("Error converting image")
+				os.Exit(1)
+			}
+
+			// Quantize the image
+			quantized := quantize(pixels, height, width, uint8(threshold))
+
+			// Print the image using halfblocks
+			printHalfBlocks(quantized, height, width)
+
+		case "truecolor":
+			// Convert the image to RGB array
+			R, G, B, height, width, err := importFrameColor(frame)
+			if err != nil {
+				fmt.Println("Error converting image")
+				os.Exit(1)
+			}
+
+			// Print the image using halfblocks, shading with color
+			printHalfBlocksColor(R, G, B, height, width)
 	}
 
-	return output
 }
 
-// subsample an image by a factor of 2 vertically
-func subsample(pixels [][]uint8, height int, width int) ([][]uint8, int, int) {
-
-	// int truncate will make sure array is even
-	height /= 2
-	
-	output := make([][]uint8, height)
-	for i := range output {
-		output[i] = make([]uint8, width)
-	}
-
-	// loop through the pixels and subsample them
-	for y := 0; y < height; y ++ {
-		for x := 0; x < width; x++ {
-			output[y][x] = pixels[y*2][x]/2 + pixels[y*2+1][x]/2
-		}
-	}
-
-	return output, height, width
+// Gets the length and width of the terminal
+func getTerminalSize() (height, width int) {
+	width, height = screen.Size()
+	return
 }
 
-
-// Print out the image using colored spaces, doubling width for perfect pixels
+// Print out the image using black and white spaces, doubling width for perfect pixels
 func printFullBlocks(pixels [][]uint8, height int, width int, repeat int) {
 	// Use a buffer of bytes to store the printed output
 	buffer := make([]byte, 0, width*height*8*2)
@@ -521,6 +238,106 @@ func printFullBlocks(pixels [][]uint8, height int, width int, repeat int) {
 	}
 
 	fmt.Print(string(buffer))
+}
+
+// Quantize an image array with a given threshold into a 2D array of 0s and 1s
+func quantize(pixels [][]uint8, height int, width int, threshold uint8) [][]uint8 {
+
+	output := make([][]uint8, height)
+	for i := range output {
+		output[i] = make([]uint8, width)
+	}
+	
+	// loop through the pixels and quantize them
+	for y := 0; y < height; y++ {
+		for x := 0; x < width; x++ {
+			if pixels[y][x] < threshold {
+				output[y][x] = 0
+			} else {
+				output[y][x] = 1
+			}
+		}
+	}
+
+	return output
+}
+
+// Reads an image and returns a 2D array of the pixels in grayscale
+func importFrame(image image.Image) ([][]uint8, int, int, error) {
+
+	// Get the image bounds
+	bounds := image.Bounds()
+	width, height := bounds.Max.X, bounds.Max.Y
+
+	// Create a 2D array of the pixels
+	pixels := make([][]uint8, height)
+	for i := range pixels {
+		pixels[i] = make([]uint8, width)
+	}
+
+	// Iterate over the pixels and convert them to grayscale
+	for y := 0; y < height; y++ {
+		for x := 0; x < width; x++ {
+			pixels[y][x] = color.GrayModel.Convert(image.At(x, y)).(color.Gray).Y
+		}
+	}
+
+	return pixels, height, width, nil
+}
+
+// Reads an image and return 3 2D arrays of the pixels in RGB, uint32
+func importFrameColor(image image.Image) ([][]uint8, [][]uint8, [][]uint8, int, int, error) {
+	
+	// Get the image bounds
+	bounds := image.Bounds()
+	width, height := bounds.Max.X, bounds.Max.Y
+
+	// Create a 2D array of the pixels
+	r := make([][]uint8, height)
+	g := make([][]uint8, height)
+	b := make([][]uint8, height)
+	a := make([][]uint8, height)
+
+	for i := range r {
+		r[i] = make([]uint8, width)
+		g[i] = make([]uint8, width)
+		b[i] = make([]uint8, width)
+		a[i] = make([]uint8, width)
+	}
+
+	// Iterate over the pixels and calculate the non premultiplied values
+	for y := 0; y < height; y++ {
+		for x := 0; x < width; x++ {
+			rgba := color.RGBAModel.Convert(image.At(x, y)).(color.RGBA)
+			r[y][x] = rgba.R
+			g[y][x] = rgba.G
+			b[y][x] = rgba.B
+			a[y][x] = rgba.A
+		}
+	}
+
+	return r, g, b, height, width, nil
+}
+
+// subsample an image by a factor of 2 vertically
+func subsample(pixels [][]uint8, height int, width int) ([][]uint8, int, int) {
+
+	// int truncate will make sure array is even
+	height /= 2
+	
+	output := make([][]uint8, height)
+	for i := range output {
+		output[i] = make([]uint8, width)
+	}
+
+	// loop through the pixels and subsample them
+	for y := 0; y < height; y ++ {
+		for x := 0; x < width; x++ {
+			output[y][x] = pixels[y*2][x]/2 + pixels[y*2+1][x]/2
+		}
+	}
+
+	return output, height, width
 }
 
 // Print out the image using half blocks
@@ -646,8 +463,8 @@ func printHalfBlocksColor(r, g, b [][]uint8, height, width int) {
 			
 			// Append the block type to the buffer
 			buffer = append(buffer, block...)
-
 		}
+
 		// Append a newline character after each row of blocks
 		buffer = append(buffer, newline...)
 
